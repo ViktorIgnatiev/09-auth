9d33bcad364394b5978a363c05dd3b0b1c040013
ViktorIgnatiev <viktor.ignatiev@wp.pl>
2025-08-05 22:08:22 +0200

    [id] props

--- app/notes/[id]/page.tsx
+++ app/notes/[id]/page.tsx
@@ -3,21 +3,19 @@ import getQueryClient from '@/utils/getQueryClient'
 import { fetchNoteById } from '@/lib/api/note'
 import NoteDetailsClient from './NoteDetails.client'
 
-export default async function NoteDetailsPage({
-  params
-}: {
-  params: { id: string }
-}) {
+export default async function NoteDetailsPage({ params }: { params: { id: string } })
+{
   const queryClient = getQueryClient()
+  const id = Number(params.id)
 
   await queryClient.prefetchQuery({
-    queryKey: ['note', params.id],
-    queryFn: () => fetchNoteById(params.id),
+    queryKey: ['note', id],
+    queryFn: () => fetchNoteById(id),
   })
 
   return (
     <HydrationBoundary state={dehydrate(queryClient)}>
-      <NoteDetailsClient id={params.id} />
+      <NoteDetailsClient id={id} />
     </HydrationBoundary>
   )
 }
